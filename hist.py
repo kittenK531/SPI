5b928f1e98f62f06edef65e1e9a0be0dd41ef7ba
kittenK531 <k.space.infinity@gmail.com>
2022-10-01 18:19:32 +0800

    histogram plot

---
+++ hist.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+print([item.get_text() for item in ax.get_xticklabels()])
+
+fig.canvas.draw()
+labels = [item.get_text() for item in ax.get_xticklabels()]
+labels[1] = -1
+labels[4] = 0
+labels[5] = +1
+
+
+zeroth = 3.454 * np.ones(78)
+posone = 4.1 * np.ones(13)
+negone = 2.81 * np.ones(18)
+
+cent = np.append(zeroth, posone)
+cent = np.append(cent, negone)
+
+near = np.append( 2.853 * np.ones(20),  3.4648* np.ones(25))
+near = np.append(near, 4.1435*np.ones(26))
+
+far_ = np.append( 3.371* np.ones(20), 2.766 * np.ones(21))
+far_ = np.append(far_,  4.056* np.ones(18))
+
+ax.grid(axis='y')
+
+ax.hist(cent, bins=30, label = "Central Maximum", color="#1f77b4")
+ax.hist(near, bins=30, label = "Near slit diffraction", color = "#ff7f0e")
+ax.hist(far_, bins=30, label = "Far Slit diffraction", color="#2ca02c")
+ax.set_xticklabels(['',r'$-1$','','','0','','',r'$+1$',''])
+ax.legend(loc='best')
+plt.title("Photo count rate as a function of detection position")
+plt.xlabel(r"Double-slit position in terms of order of period ($T/2$)")
+plt.ylabel(r"Photon count rate from PCIT ($count/s$)")
+plt.show(block=False)
+plt.savefig("plots/pdf/histogram/quantum_paradox.pdf")
